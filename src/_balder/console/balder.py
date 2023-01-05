273ac158415cb7ae9e68390bae2a70746a9cb838
Max Stahlschmidt <max@stahl-schmidt.de>
2023-01-05 19:09:11 +0100

    feat(console.balder): split up main balder entrypoint to support internal error capturing and better test debugging
    added a new function `_console_balder_debug` that allows more debugging interfaces

--- src/_balder/console/balder.py
+++ src/_balder/console/balder.py
@@ -1,9 +1,40 @@
 from __future__ import annotations
 
+import pathlib
+import sys
+import traceback
+from typing import Callable, Optional, Union, List
+from _balder.exceptions import BalderException
 from _balder.balder_session import BalderSession
 
 
-def console_balder():
+def console_balder(cmd_args: Optional[List[str]] = None, working_dir: Union[str, pathlib.Path, None] = None):
     """script that executes a balder session"""
-    balder_session = BalderSession()
-    balder_session.run()
+    _console_balder_debug(cmd_args=cmd_args, working_dir=working_dir)
+
+
+def _console_balder_debug(cmd_args: Optional[List[str]] = None, working_dir: Union[str, pathlib.Path, None] = None,
+                          cb_session_created: Optional[Callable] = None, cb_run_finished: Optional[Callable] = None,
+                          cb_balder_exc: Optional[Callable] = None, cb_unexpected_exc: Optional[Callable] = None):
+    """helper balder execution that allows more debug access"""
+    try:
+        balder_session = BalderSession(cmd_args=cmd_args, working_dir=working_dir)
+
+        if cb_session_created:
+            cb_session_created(balder_session)
+
+        balder_session.run()
+
+        if cb_run_finished:
+            cb_run_finished(balder_session)
+
+    except BalderException as exc:
+        # a balder usage error occurs
+        if cb_balder_exc:
+            cb_balder_exc(exc)
+        traceback.print_exception(*sys.exc_info())
+    except Exception as exc:
+        # a unexpected error occurs
+        if cb_unexpected_exc:
+            cb_unexpected_exc(exc)
+        traceback.print_exception(*sys.exc_info())
