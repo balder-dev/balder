cd6af4dafff3f67ecf0810c5843c050a740126b2
Max Stahlschmidt <max@stahl-schmidt.de>
2023-01-06 16:32:50 +0100

    feat(test_0_scenario_inheritance_overwrite_only_one_device.py): set expected exit code to 4 (is a balder environment error)

--- tests/scenario_inheritance/test_0_scenario_overwrite_only_one_device/test_0_scenario_inheritance_overwrite_only_one_device.py
+++ tests/scenario_inheritance/test_0_scenario_overwrite_only_one_device/test_0_scenario_inheritance_overwrite_only_one_device.py
@@ -20,6 +20,10 @@ class Test0ScenarioInheritanceOverwriteOnlyOneDevice(Base0EnvtesterClass):
     def expected_data(self) -> tuple:
         return ()
 
+    @property
+    def expected_exit_code(self) -> int:
+        return 4
+
     @staticmethod
     def handle_balder_exception(exc: BalderException):
         assert isinstance(exc, DeviceOverwritingError), 'unexpected exception type'
