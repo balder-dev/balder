2295f2be993c25e112189fa9a9ddf8718f928c45
Max Stahlschmidt <max@stahl-schmidt.de>
2023-01-06 15:08:51 +0100

    tests(test_01_envtester_methvar): rework test and introduce new base class that handles everything for envtester_methvar environments
    parent base class `Base01EnvtesterMetvarClass` allows better and nicer creation of tests

---
+++ tests/test_utilities/base_01_envtester_methvar_class.py
@@ -0,0 +1,9 @@
+from abc import ABC
+from .base_0_envtester_class import Base0EnvtesterClass
+
+
+class Base01EnvtesterMethvarClass(Base0EnvtesterClass, ABC):
+    """
+    This is a base class for all tests that want to use the 01_envtester_methvar environment.
+    """
+    pass
