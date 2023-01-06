0f23734a56e02f7d0c8239ee9ab43b519b03c8c1
Max Stahlschmidt <max@stahl-schmidt.de>
2023-01-06 16:24:03 +0100

    feat(exit_code): add new `ExitCode` enum into main project and integrate it into `BalderSession` and main balder script

---
+++ src/_balder/exit_code.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class ExitCode(Enum):
+    #: tests were collected and all tests and fixtures terminates with success
+    SUCCESS = 0
+    #: tests were collected but some tests failed
+    TESTS_FAILED = 1
+    #: test was interrupted by user
+    USER_INTERRUPT = 2
+    #: an internal unexpected error occurs
+    UNEXPECTED_ERROR = 3
+    #: Balder usage error
+    BALDER_USAGE_ERROR = 4
+    #: no tests were collected
+    NO_TESTS_COLLECTED = 5
