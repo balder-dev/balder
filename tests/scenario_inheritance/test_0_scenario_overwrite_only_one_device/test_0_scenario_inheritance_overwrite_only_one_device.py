3973ce10db99ecb2e1c962d4c25ac816c606bb8f
Max Stahlschmidt <max@stahl-schmidt.de>
2023-01-06 13:09:13 +0100

    tests(test_0_scenario_inheritance_overwrite_only_one_device.py): adjust testcase for new `Base0EnvtesterClass` base class
    makes the test easier to understand and also uses the new queue system to communicate with the main process

--- tests/scenario_inheritance/test_0_scenario_overwrite_only_one_device/test_0_scenario_inheritance_overwrite_only_one_device.py
+++ tests/scenario_inheritance/test_0_scenario_overwrite_only_one_device/test_0_scenario_inheritance_overwrite_only_one_device.py
@@ -1,10 +1,10 @@
-
-from multiprocessing import Process
-from balder.exceptions import DeviceOverwritingError
+from balder.exceptions import DeviceOverwritingError, BalderException
 from _balder.balder_session import BalderSession
 
+from tests.test_utilities.base_0_envtester_class import Base0EnvtesterClass
 
-def test_0_scenario_inheritance_overwrite_only_one_device(balder_working_dir):
+
+class Test0ScenarioInheritanceOverwriteOnlyOneDevice(Base0EnvtesterClass):
     """
     This testcase executes a reduced version of the basic envtester environment. It only implements the `ScenarioA` and
     its related `SetupA` and a child class of the related ScenarioA that has an additional test method.
@@ -16,22 +16,17 @@ def test_0_scenario_inheritance_overwrite_only_one_device(balder_working_dir):
         The `ScenarioAParent` class has a child (the `ScenarioAChild` class). This forbids the execution of the
         `ScenarioAParent` class.
     """
-    proc = Process(target=processed, args=(balder_working_dir, ))
-    proc.start()
-    proc.join()
-    assert proc.exitcode == 0, "the process terminates with an error"
+    @property
+    def expected_data(self) -> tuple:
+        return ()
 
-
-def processed(env_dir):
-
-    print("\n", flush=True)
-    session = BalderSession(cmd_args=[], working_dir=env_dir)
-    try:
-        session.run()
-        print("\n")
-        assert False, "test session terminates without an error"
-    except DeviceOverwritingError as exc:
+    @staticmethod
+    def handle_balder_exception(exc: BalderException):
+        assert isinstance(exc, DeviceOverwritingError), 'unexpected exception type'
         assert exc.args[0] == "found a device `ScenarioAParent.ScenarioDevice2` which is part of a parent class, " \
                               "but it is not implemented in child class `ScenarioAChild`"
 
-    assert session.executor_tree is None, "test session does not terminates before collector work was done"
+    @staticmethod
+    def validate_finished_session(session: BalderSession):
+        assert session.executor_tree is None, "test session does not terminates before collector work was done"
+
