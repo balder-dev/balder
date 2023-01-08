f7ac29e41674a058660853a640f3417d985d6070
Max Stahlschmidt <max@stahl-schmidt.de>
2023-01-08 09:32:36 +0100

    refactor(balder/exceptions.py): fix pylint message `wildcard-import` and do not use wildcard imports

--- src/balder/exceptions.py
+++ src/balder/exceptions.py
@@ -1,4 +1,12 @@
-from _balder.exceptions import *
+from _balder.exceptions import BalderException, BalderWarning
+from _balder.exceptions import FixtureScopeError, FixtureReferenceError, UnclearSetupScopedFixtureReference, \
+    UnclearUniqueClassReference, LostInExecutorTreeException, DeviceResolvingException, NodeNotExistsError, \
+    DuplicateForVDeviceError, DuplicateBalderSettingError, DeviceOverwritingError, VDeviceOverwritingError, \
+    FeatureOverwritingError, UnknownVDeviceException, RoutingBrokenChainError, IllegalConnectionTypeError, \
+    ConnectionMetadataConflictError, DeviceScopeError, ConnectionIntersectionError, \
+    UnclearAssignableFeatureConnectionError, InheritanceError, MultiInheritanceError, InnerFeatureResolvingError, \
+    VDeviceResolvingError, IllegalVDeviceMappingError, NotApplicableVariationError, UnclearMethodVariationError, \
+    UnexpectedPluginMethodReturnValue
 
 __all__ = [
 
