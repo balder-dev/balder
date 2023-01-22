ee57216464fc755c35108955753126f4398420eb
Max Stahlschmidt <max@stahl-schmidt.de>
2023-01-22 20:39:27 +0100

    tests(test_0_new_setup_only_device_feature.py): add new test that secures the functionality and usability of setup-only defined features

---
+++ tests/device/test_0_new_setup_only_device_feature/test_0_new_setup_only_device_feature.py
@@ -0,0 +1,184 @@
+from _balder.testresult import ResultState
+from _balder.balder_session import BalderSession
+from ...test_utilities.base_0_envtester_class import Base0EnvtesterClass
+
+
+class Test0NewSetupOnlyDeviceFeature(Base0EnvtesterClass):
+    """
+    This testcase is a simplified version of the basic envtester environment. It only uses `ScenarioA` and `SetupA`. The
+    device `ScenarioA.ScenarioDevice1` doesn't define any features by itself, while the related setup device
+    `SetupA.SetupDevice1` uses a completely new defined feature `SetupFeatureI` which is not defined in any
+    scenario-device. These both devices should match with each other while the ``SetupFeatureI`` can be fully used by
+    all setup fixtures.
+
+    The test should terminate successfully.
+    """
+
+    @property
+    def expected_data(self):
+        return (
+            # FIXTURE-CONSTRUCTION: balderglob_fixture_session
+            {"file": "balderglob.py", "meth": "balderglob_fixture_session", "part": "construction"},
+            # FIXTURE-CONSTRUCTION: SetupA.fixture_session
+            {"cls": "SetupA", "meth": "fixture_session", "part": "construction"},
+            {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-CONSTRUCTION: ScenarioA.fixture_session
+            {"cls": "ScenarioA", "meth": "fixture_session", "part": "construction"},
+            {"cls": "FeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-CONSTRUCTION: balderglob_fixture_setup
+            {"file": "balderglob.py", "meth": "balderglob_fixture_setup", "part": "construction"},
+            # FIXTURE-CONSTRUCTION: SetupA.fixture_setup
+            {"cls": "SetupA", "meth": "fixture_setup", "part": "construction"},
+            {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-CONSTRUCTION: ScenarioA.fixture_setup
+            {"cls": "ScenarioA", "meth": "fixture_setup", "part": "construction"},
+            {"cls": "FeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-CONSTRUCTION: balderglob_fixture_scenario
+            {"file": "balderglob.py", "meth": "balderglob_fixture_scenario", "part": "construction"},
+            # FIXTURE-CONSTRUCTION: SetupA.fixture_scenario
+            {"cls": "SetupA", "meth": "fixture_scenario", "part": "construction"},
+            {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-CONSTRUCTION: ScenarioA.fixture_scenario
+            {"cls": "ScenarioA", "meth": "fixture_scenario", "part": "construction"},
+            {"cls": "FeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-CONSTRUCTION: balderglob_fixture_variation
+            {"file": "balderglob.py", "meth": "balderglob_fixture_variation", "part": "construction"},
+            # FIXTURE-CONSTRUCTION: SetupA.fixture_variation
+            {"cls": "SetupA", "meth": "fixture_variation", "part": "construction"},
+            {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-CONSTRUCTION: ScenarioA.fixture_variation
+            {"cls": "ScenarioA", "meth": "fixture_variation", "part": "construction"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            [
+                (
+                    # FIXTURE-CONSTRUCTION: balderglob_fixture_testcase
+                    {"file": "balderglob.py", "meth": "balderglob_fixture_testcase", "part": "construction"},
+                    # FIXTURE-CONSTRUCTION: SetupA.fixture_testcase
+                    {"cls": "SetupA", "meth": "fixture_testcase", "part": "construction"},
+                    {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # FIXTURE-CONSTRUCTION: ScenarioA.fixture_testcase
+                    {"cls": "ScenarioA", "meth": "fixture_testcase", "part": "construction"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # TESTCASE: ScenarioA.test_a_1
+                    {"cls": "ScenarioA", "meth": "test_a_1"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # FIXTURE-TEARDOWN: ScenarioA.fixture_testcase
+                    {"cls": "ScenarioA", "meth": "fixture_testcase", "part": "teardown"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # FIXTURE-TEARDOWN: SetupA.fixture_testcase
+                    {"cls": "SetupA", "meth": "fixture_testcase", "part": "teardown"},
+                    {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # FIXTURE-TEARDOWN: balderglob_fixture_testcase
+                    {"file": "balderglob.py", "meth": "balderglob_fixture_testcase", "part": "teardown"},
+                ),
+                (
+                    # FIXTURE-CONSTRUCTION: balderglob_fixture_testcase
+                    {"file": "balderglob.py", "meth": "balderglob_fixture_testcase", "part": "construction"},
+                    # FIXTURE-CONSTRUCTION: SetupA.fixture_testcase
+                    {"cls": "SetupA", "meth": "fixture_testcase", "part": "construction"},
+                    {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # FIXTURE-CONSTRUCTION: ScenarioA.fixture_testcase
+                    {"cls": "ScenarioA", "meth": "fixture_testcase", "part": "construction"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # TESTCASE: ScenarioA.test_a_2
+                    {"cls": "ScenarioA", "meth": "test_a_2"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # FIXTURE-TEARDOWN: ScenarioA.fixture_testcase
+                    {"cls": "ScenarioA", "meth": "fixture_testcase", "part": "teardown"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # FIXTURE-TEARDOWN: SetupA.fixture_testcase
+                    {"cls": "SetupA", "meth": "fixture_testcase", "part": "teardown"},
+                    {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+                    {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+                    # FIXTURE-TEARDOWN: balderglob_fixture_testcase
+                    {"file": "balderglob.py", "meth": "balderglob_fixture_testcase", "part": "teardown"},
+                ),
+            ],
+            # FIXTURE-TEARDOWN: ScenarioA.fixture_variation
+            {"cls": "ScenarioA", "meth": "fixture_variation", "part": "teardown"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-TEARDOWN: SetupA.fixture_variation
+            {"cls": "SetupA", "meth": "fixture_variation", "part": "teardown"},
+            {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-TEARDOWN: balderglob_fixture_variation
+            {"file": "balderglob.py", "meth": "balderglob_fixture_variation", "part": "teardown"},
+            # FIXTURE-TEARDOWN: ScenarioA.fixture_scenario
+            {"cls": "ScenarioA", "meth": "fixture_scenario", "part": "teardown"},
+            {"cls": "FeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-TEARDOWN: SetupA.fixture_scenario
+            {"cls": "SetupA", "meth": "fixture_scenario", "part": "teardown"},
+            {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-TEARDOWN: balderglob_fixture_scenario
+            {"file": "balderglob.py", "meth": "balderglob_fixture_scenario", "part": "teardown"},
+            # FIXTURE-TEARDOWN: ScenarioA.fixture_setup
+            {"cls": "ScenarioA", "meth": "fixture_setup", "part": "teardown"},
+            {"cls": "FeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-TEARDOWN: SetupA.fixture_setup
+            {"cls": "SetupA", "meth": "fixture_setup", "part": "teardown"},
+            {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-TEARDOWN: balderglob_fixture_setup
+            {"file": "balderglob.py", "meth": "balderglob_fixture_setup", "part": "teardown"},
+            # FIXTURE-TEARDOWN: ScenarioA.fixture_session
+            {"cls": "ScenarioA", "meth": "fixture_session", "part": "teardown"},
+            {"cls": "FeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-TEARDOWN: SetupA.fixture_session
+            {"cls": "SetupA", "meth": "fixture_session", "part": "teardown"},
+            {"cls": "SetupFeatureI", "meth": "do_something", "category": "feature"},
+            {"cls": "SetupFeatureII", "meth": "do_something", "category": "feature"},
+            # FIXTURE-TEARDOWN: balderglob_fixture_session
+            {"file": "balderglob.py", "meth": "balderglob_fixture_session", "part": "teardown"},
+        )
+
+    @staticmethod
+    def validate_finished_session(session: BalderSession):
+        # check result states everywhere (have to be SUCCESS everywhere
+        assert session.executor_tree.executor_result == ResultState.SUCCESS, \
+            "test session does not terminates with success"
+
+        assert session.executor_tree.construct_result.result == ResultState.SUCCESS, \
+            "global executor tree construct part does not set ResultState.SUCCESS"
+        assert session.executor_tree.body_result.result == ResultState.SUCCESS, \
+            "global executor tree body part does not set ResultState.SUCCESS"
+        assert session.executor_tree.teardown_result.result == ResultState.SUCCESS, \
+            "global executor tree teardown part does not set ResultState.SUCCESS"
+        for cur_setup_executor in session.executor_tree.setup_executors:
+            assert cur_setup_executor.executor_result == ResultState.SUCCESS, \
+                "the setup executor does not have result SUCCESS"
+
+            assert cur_setup_executor.construct_result.result == ResultState.SUCCESS
+            assert cur_setup_executor.body_result.result == ResultState.SUCCESS
+            assert cur_setup_executor.teardown_result.result == ResultState.SUCCESS
+
+            for cur_scenario_executor in cur_setup_executor.scenario_executors:
+                assert cur_scenario_executor.executor_result == ResultState.SUCCESS, \
+                    "the scenario executor does not have result SUCCESS"
+
+                assert cur_scenario_executor.construct_result.result == ResultState.SUCCESS
+                assert cur_scenario_executor.body_result.result == ResultState.SUCCESS
+                assert cur_scenario_executor.teardown_result.result == ResultState.SUCCESS
+
+                for cur_variation_executor in cur_scenario_executor.variation_executors:
+                    assert cur_variation_executor.executor_result == ResultState.SUCCESS, \
+                        "the variation executor does not have result SUCCESS"
+
+                    assert cur_variation_executor.construct_result.result == ResultState.SUCCESS
+                    assert cur_variation_executor.body_result.result == ResultState.SUCCESS
+                    assert cur_variation_executor.teardown_result.result == ResultState.SUCCESS
+
+                    for cur_testcase_executor in cur_variation_executor.testcase_executors:
+                        assert cur_testcase_executor.executor_result == ResultState.SUCCESS, \
+                            "the testcase executor does not have result SUCCESS"
+
+                        assert cur_testcase_executor.construct_result.result == ResultState.SUCCESS
+                        assert cur_testcase_executor.body_result.result == ResultState.SUCCESS
+                        assert cur_testcase_executor.teardown_result.result == ResultState.SUCCESS
