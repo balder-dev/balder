9570e399f36e9dbc000f1ce9a53e84ef87a20c67
Max Stahlschmidt <max@stahl-schmidt.de>
2023-01-27 12:55:49 +0100

    tests(test_1_reverse_connect_3_scen_4_setup_devs.py): add new test that checks if the routing works as expected for multiple devices (>3) with the same connections

---
+++ tests/connection/test_1_reverse_connect_3_scen_4_setup_devs/env/setups/setup_pythonadd.py
@@ -0,0 +1,26 @@
+import balder
+from ..lib.connections import MySimplySharedMemoryConnection
+from ..lib.utils import SharedObj
+from .features_setup import PyAddCalculate, PyAddProvideANumber
+
+
+class SetupPythonAdd(balder.Setup):
+
+    class Calculator(balder.Device):
+        calc = PyAddCalculate()
+
+    @balder.connect(Calculator, over_connection=MySimplySharedMemoryConnection)
+    class NumberProvider1(balder.Device):
+        n = PyAddProvideANumber()
+
+    @balder.connect(Calculator, over_connection=MySimplySharedMemoryConnection)
+    class NumberProvider2(balder.Device):
+        n = PyAddProvideANumber()
+
+    @balder.connect(Calculator, over_connection=MySimplySharedMemoryConnection)
+    class NumberProvider3(balder.Device):
+        n = PyAddProvideANumber()
+
+    @balder.fixture(level="testcase")
+    def cleanup_memory(self):
+        SharedObj.shared_mem_list = []
---
+++ tests/connection/test_1_reverse_connect_3_scen_4_setup_devs/test_1_reverse_connect_3_scen_4_setup_devs.py
@@ -0,0 +1,33 @@
+from multiprocessing import Process
+
+from _balder.testresult import ResultState
+from _balder.balder_session import BalderSession
+
+
+def test_1_reverse_connect_3_scen_4_setup_devs(balder_working_dir):
+    """
+    This testcase uses the calculator environment in a modified version. It gets an additional `NumberProvider3` on
+    setup level. On setup level all connections are defined over the `Calculator` (the connections are defined reverse).
+
+    The test validates that the session terminates successfully and all three variations were executed.
+    """
+    proc = Process(target=processed, args=(balder_working_dir, ))
+    proc.start()
+    proc.join()
+    assert proc.exitcode == 0, "the process terminates with an error"
+
+
+def processed(env_dir):
+    print("\n", flush=True)
+    session = BalderSession(cmd_args=[], working_dir=env_dir)
+    session.run()
+    print("\n")
+
+    assert session.executor_tree.executor_result == ResultState.SUCCESS, \
+        "test session does not terminates with success"
+    assert len(session.executor_tree.setup_executors) == 1, \
+        "found not exactly one setup executor"
+    assert len(session.executor_tree.setup_executors[0].scenario_executors) == 1, \
+        "found not exactly one scenario executor"
+    assert len(session.executor_tree.setup_executors[0].scenario_executors[0].variation_executors) == 6, \
+        "found not exactly three variation executor"
