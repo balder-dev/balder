34917d26ce585723978004a235ef8d7fa06c9c32
matosys <max@matosys.de>
2023-03-05 10:36:58 +0100

    docs(conf.py): fix problem with `sphinxcontrib.mermaid` and use static js min file
    there were problems with the file provider - download minified version and provide them by sphinx itself

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -71,3 +71,11 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- mermaid
+
+mermaid_version = ""
+
+html_js_files = [
+   'js/mermaid.js',
+]
