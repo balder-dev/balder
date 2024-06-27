b9737f8b2cad7973a9b7656377335f70ea760557
matosys <max@matosys.de>
2024-06-27 08:43:34 +0400

    tests(test_0_inner_scenario_feature_reference.py): add new test that validates the assignment of inner scenario features

---
+++ tests/features/inner_feature_references/test_0_inner_scenario_feature_ref/env/lib/features.py
@@ -0,0 +1,53 @@
+import balder
+from ..balderglob import RuntimeObserver
+
+
+class FeatureI(balder.Feature):
+
+    def do_something(self):
+        RuntimeObserver.add_entry(
+            __file__, FeatureI, FeatureI.do_something, "enter `FeatureI.do_something`", category="feature")
+
+    def called_from_outer_feature(self):
+        RuntimeObserver.add_entry(
+            __file__, FeatureI, FeatureI.called_from_outer_feature, "enter `FeatureI.called_from_outer_feature`",
+            category="feature")
+
+
+class FeatureII(balder.Feature):
+    feature_i = FeatureI()
+
+    def do_something(self):
+        RuntimeObserver.add_entry(
+            __file__, FeatureII, FeatureII.do_something, "enter `FeatureII.do_something`", category="feature")
+        self.feature_i.called_from_outer_feature()
+
+    def called_from_outer_feature(self):
+        RuntimeObserver.add_entry(
+            __file__, FeatureI, FeatureI.called_from_outer_feature, "enter `FeatureII.called_from_outer_feature`",
+            category="feature")
+
+
+class FeatureIII(balder.Feature):
+
+    def do_something(self):
+        RuntimeObserver.add_entry(
+            __file__, FeatureIII, FeatureIII.do_something, "enter `FeatureIII.do_something`", category="feature")
+
+    def called_from_outer_feature(self):
+        RuntimeObserver.add_entry(
+            __file__, FeatureIV, FeatureIV.called_from_outer_feature, "enter `FeatureIII.called_from_outer_feature`",
+            category="feature")
+
+
+class FeatureIV(balder.Feature):
+
+    def do_something(self):
+        RuntimeObserver.add_entry(
+            __file__, FeatureIV, FeatureIV.do_something, "enter `FeatureIV.do_something`", category="feature")
+
+    def called_from_outer_feature(self):
+        RuntimeObserver.add_entry(
+            __file__, FeatureIV, FeatureIV.called_from_outer_feature, "enter `FeatureIV.called_from_outer_feature`",
+            category="feature")
+
