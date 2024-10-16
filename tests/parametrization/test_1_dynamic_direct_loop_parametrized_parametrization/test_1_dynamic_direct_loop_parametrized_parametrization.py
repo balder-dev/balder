5ea8be15ac8eda5b0ebeb0ac0abfe2eb78b11644
matosys <max@matosys.de>
2024-10-16 15:47:08 +0400

    tests(test_1_dynamic_direct_loop_parametrized_parametrization.py): introduce new test that checks that a direct loop with parametrized `Parameter` objects are created

---
+++ tests/parametrization/test_1_dynamic_direct_loop_parametrized_parametrization/test_1_dynamic_direct_loop_parametrized_parametrization.py
@@ -0,0 +1,28 @@
+from multiprocessing import Process
+
+from _balder.testresult import ResultState
+from _balder.balder_session import BalderSession
+
+
+def test_1_calculator(balder_working_dir):
+    """
+    This testcase executes the basic example and checks if the tree ends with the result SUCCESS
+    """
+    proc = Process(target=processed, args=(balder_working_dir, ))
+    proc.start()
+    proc.join()
+    assert proc.exitcode == 0, "the process terminates with an error"
+
+
+def processed(env_dir):
+    print("\n", flush=True)
+    session = BalderSession(cmd_args=[], working_dir=env_dir)
+    try:
+        session.run()
+        print("\n")
+        assert False, "test session terminates without an error"
+    except AttributeError as exc:
+        assert exc.args[0] == "detect a loop in Parameter() object - can not apply parametrization"
+
+    assert session.executor_tree is None, "test session does not terminates before collector work was done"
+
