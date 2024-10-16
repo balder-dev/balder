41ed2902d321a8064dea6ce0f8f1c6864e71b46b
matosys <max@matosys.de>
2024-10-16 15:50:59 +0400

    tests(test_1_dynamic_3pointloop_parametrized_parametrization.py): introduce new test that checks that a loop with parametrized `Parameter` objects over 3 parameters are created

---
+++ tests/parametrization/test_1_dynamic_3pointloop_parametrized_parametrization/env/scenarios/scenario_adding.py
@@ -0,0 +1,34 @@
+import balder
+from balder.parametrization import Parameter
+from ..lib.features import AddCalculateFeature, ProvidesANumberFeature, RandomNumberFeature
+
+
+class ScenarioAdding(balder.Scenario):
+
+    class Calculator(balder.Device):
+        # we could add vDevices here later too -> f.e. `NumberOne="NumberOneDevice", NumberTwo="NumberTwoDevice"`
+        adds = AddCalculateFeature()
+
+    @balder.connect(Calculator, over_connection=balder.Connection)
+    class NumberOneDevice(balder.Device):
+        number = ProvidesANumberFeature()
+        example = RandomNumberFeature()
+
+    @balder.connect(Calculator, over_connection=balder.Connection)
+    class NumberTwoDevice(balder.Device):
+        number = ProvidesANumberFeature()
+        example = RandomNumberFeature()
+
+    @balder.parametrize_by_feature('number1', (NumberOneDevice, 'example', 'get_random_numbers_higher_than'), parameter={'value': Parameter('number2')})
+    @balder.parametrize_by_feature('number2', (NumberTwoDevice, 'example', 'get_random_numbers_higher_than'), parameter={'value': Parameter('number3')})
+    @balder.parametrize_by_feature('number3', (NumberTwoDevice, 'example', 'get_random_numbers_higher_than'), parameter={'value': Parameter('number1')})
+    def test_add_two_numbers(self, number1, number2, number3):
+        self.NumberOneDevice.number.set_number(number1 + number3)
+        self.NumberTwoDevice.number.set_number(number2)
+
+        self.NumberOneDevice.number.sends_the_number()
+        self.NumberTwoDevice.number.sends_the_number()
+
+        self.Calculator.adds.get_numbers()
+        result = self.Calculator.adds.add_numbers()
+        assert result == number1 + number2 + number3, "result is not as expected"
