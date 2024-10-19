844436a153e88a41194940ae99535b9050122d08
matosys <max@matosys.de>
2024-10-19 09:50:54 +0400

    tests(test_1_dynamic_parametrizing_no_iterable.py): introduce new test that validates a dynamic parametrized test while the parametrization returns no iterable

---
+++ tests/parametrization/test_1_dynamic_parametrizing_no_iterable/env/lib/features.py
@@ -0,0 +1,39 @@
+import balder
+import random
+
+
+class ProvidesANumberFeature(balder.Feature):
+
+    the_number = 0
+
+    def set_number(self, number):
+        """user method that allows to set the number"""
+        self.the_number = number
+
+    def get_number(self):
+        """returns the set number"""
+        raise NotImplementedError("has to be implemented in subclass")
+
+    def sends_the_number(self):
+        """sends the set number"""
+        raise NotImplementedError("has to be implemented in subclass")
+
+
+class RandomNumberFeature(balder.Feature):
+
+    def get_random_numbers(self):
+        # is used for parametrization - but should not be possible normally
+        return random.randint(1, 100)
+
+
+class AddCalculateFeature(balder.Feature):
+
+    all_numbers = []
+
+    def get_numbers(self):
+        """this method get all the single numbers that should be calculated"""
+        raise NotImplementedError("has to be implemented in subclass")
+
+    def add_numbers(self):
+        """this method adds all the numbers"""
+        raise NotImplementedError("has to be implemented in subclass")
