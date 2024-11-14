3a53fa6fea043f79b352772ecbf61e2d3572fd4d
matosys <max@matosys.de>
2024-11-14 19:05:55 +0400

    refactor(cnnrelations): add new AndConnectionRelation and OrConnectionRelation objects for managing connections

---
+++ src/_balder/cnnrelations/and_connection_relation.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from .base_connection_relation import BaseConnectionRelation
+
+
+class AndConnectionRelation(BaseConnectionRelation):
+
+    def get_tree_str(self) -> str:
+        based_on_strings = [cur_elem.get_tree_str() for cur_elem in self._connections]
+        return f"({' | '.join(based_on_strings)})"
