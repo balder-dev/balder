9884b13db553c5d35754c227f343640607cbadff
matosys <max@matosys.de>
2024-11-14 19:05:55 +0400

    refactor(cnnrelations): add new AndConnectionRelation and OrConnectionRelation objects for managing connections

---
+++ src/_balder/cnnrelations/__init__.py
@@ -0,0 +1,7 @@
+from .and_connection_relation import AndConnectionRelation
+from .or_connection_relation import OrConnectionRelation
+
+__all__ = [
+    'AndConnectionRelation',
+    'OrConnectionRelation'
+]
---
+++ src/_balder/cnnrelations/base_connection_relation.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+from typing import List, Union, Type, TypeVar, TYPE_CHECKING
+from abc import ABC, abstractmethod
+
+if TYPE_CHECKING:
+    from ..connection import Connection
+
+BaseConnectionRelationT = TypeVar('BaseConnectionRelationT', bound='BaseConnectionRelation')
+
+
+class BaseConnectionRelation(ABC):
+    """
+    This is the base connection list class, which provides the general functionality of connection collections.
+    """
+
+    def __init__(self, *connections: Union[Type[Connection], Connection, BaseConnectionRelationT]):
+        self._connections = []
+        # add it over append for type checking
+        for connection in connections:
+            self.append(connection)
+
+    def __iter__(self):
+        return self._connections.__iter__()
+
+    def __getitem__(self, item):
+        return self._connections.__getitem__(item)
+
+    def __len__(self):
+        return len(self._connections)
+
+    @property
+    def connections(self) -> List[Union[Connection, BaseConnectionRelationT]]:
+        """
+        returns the components of this connection relation
+        """
+        return self._connections.copy()
+
+    def clone(self):
+        """
+        clones this connection relation
+        """
+        return self.__class__(*[cnn.clone() for cnn in self._connections])
+
+    def append(self, connection: Union[Type[Connection], Connection, BaseConnectionRelationT]):
+        """
+        appends a component to this connection relation
+        """
+        from ..connection import Connection
+
+        if isinstance(connection, type):
+            if issubclass(connection, Connection):
+                connection = connection()
+            else:
+                raise TypeError(f'can not append a element `{connection.__name__}`')
+        if not isinstance(connection, Connection) and not isinstance(connection, BaseConnectionRelation):
+            raise TypeError('the element that should be appended to the relation needs to be a Connection or another '
+                            'relation')
+        self._connections.append(connection)
+
+    def extend(self, relation: BaseConnectionRelationT):
+        """
+        extends the relation with the elements of provided relation
+        """
+        if not isinstance(relation, self.__class__):
+            raise TypeError(f'the relation with the elements that should be appended needs to be from same type (is '
+                            f'`{relation.__class__}` | expected `{self.__class__}`)')
+        for cur_elem in relation.connections:
+            self.append(cur_elem)
+
+    @abstractmethod
+    def get_tree_str(self) -> str:
+        """
+        returns the tree string for this part of the connection tree
+        """
