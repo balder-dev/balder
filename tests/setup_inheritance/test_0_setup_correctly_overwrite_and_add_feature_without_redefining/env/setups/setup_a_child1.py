d649fadf976f8f974e5dfdd3587c4591ab3b149a
matosys <max@matosys.de>
2025-02-06 09:10:17 +0100

    tests(setup_inheritance): introduce new test `test_0_setup_correctly_overwrite_and_add_feature_without_redefining.py`

---
+++ tests/setup_inheritance/test_0_setup_correctly_overwrite_and_add_feature_without_redefining/env/setups/setup_a_child1.py
@@ -0,0 +1,109 @@
+import balder
+import logging
+from .setup_features import SetupFeatureIOverwritten, SetupFeatureII, SetupFeatureNew
+from ..lib.connections import AConnection
+from ..balderglob import RuntimeObserver
+from .setup_a_parent import SetupAParent
+
+logger = logging.getLogger(__name__)
+
+
+class SetupAChild1(SetupAParent):
+    """This is a setup of category A (exactly the same as scenario A)"""
+
+    class SetupDevice1(SetupAParent.SetupDevice1):
+        s_i = SetupFeatureIOverwritten()
+
+    @balder.connect(SetupDevice1, over_connection=AConnection)
+    class SetupDevice2(SetupAParent.SetupDevice2):
+        new = SetupFeatureNew()
+
+    @balder.fixture(level="session")
+    def fixture_session(self):
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_session, category="fixture",
+                                  part="construction", msg="begin execution CONSTRUCTION of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
+
+        yield
+
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_session, category="fixture",
+                                  part="teardown", msg="begin execution TEARDOWN of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
+
+    @balder.fixture(level="setup")
+    def fixture_setup(self):
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_setup, category="fixture",
+                                  part="construction", msg="begin execution CONSTRUCTION of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
+
+        yield
+
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_setup, category="fixture", part="teardown",
+                                  msg="begin execution TEARDOWN of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
+
+    @balder.fixture(level="scenario")
+    def fixture_scenario(self):
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_scenario, category="fixture",
+                                  part="construction", msg="begin execution CONSTRUCTION of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
+
+        yield
+
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_scenario, category="fixture",
+                                  part="teardown", msg="begin execution TEARDOWN of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
+
+    @balder.fixture(level="variation")
+    def fixture_variation(self):
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_variation, category="fixture",
+                                  part="construction", msg="begin execution CONSTRUCTION of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
+
+        yield
+
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_variation, category="fixture",
+                                  part="teardown", msg="begin execution TEARDOWN of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
+
+    @balder.fixture(level="testcase")
+    def fixture_testcase(self):
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_testcase, category="fixture",
+                                  part="construction", msg="begin execution CONSTRUCTION of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
+
+        yield
+
+        RuntimeObserver.add_entry(__file__, SetupAChild1, SetupAChild1.fixture_testcase, category="fixture",
+                                  part="teardown", msg="begin execution TEARDOWN of fixture")
+
+        self.SetupDevice1.s_i.do_something()
+        self.SetupDevice2.s_ii.do_something()
+        self.SetupDevice2.new.do_something()
