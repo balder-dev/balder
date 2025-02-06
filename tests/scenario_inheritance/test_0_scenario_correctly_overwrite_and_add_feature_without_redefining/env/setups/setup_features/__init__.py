1e5ea36661623874191ed54186ea55b5e5fcf692
matosys <max@matosys.de>
2025-02-06 08:44:32 +0100

    tests(scenario_inheritance): introduce new test `test_0_scenario_correctly_overwrite_and_add_feature_without_redefining.py`

---
+++ tests/scenario_inheritance/test_0_scenario_correctly_overwrite_and_add_feature_without_redefining/env/setups/setup_features/__init__.py
@@ -0,0 +1,9 @@
+from .feature_i import SetupFeatureI
+from .feature_ii import SetupFeatureII
+from .newly_setup_feature import NewlySetupFeature
+
+__all__ = [
+    "SetupFeatureI",
+    "SetupFeatureII",
+    "NewlySetupFeature"
+]
