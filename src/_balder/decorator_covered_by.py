b7022d3f708a86360a6009653ba6ec66e02c918d
matosys <max@matosys.de>
2025-03-06 08:01:27 +0100

    fix(decorator_covered_by.py): update error messages and add NotImplementedError for currently unsupported scenario classes

--- src/_balder/decorator_covered_by.py
+++ src/_balder/decorator_covered_by.py
@@ -22,10 +22,10 @@ def covered_by(item: Union[Type[Scenario], str, callable, None]):
     elif callable(item) and inspect.isfunction(item) and item.__name__.startswith("test_"):
         pass
     elif isinstance(item, type) and issubclass(item, Scenario):
-        pass
+        raise NotImplementedError('The covered-by other scenario classes is not supported yet')
     else:
-        raise TypeError("the given element for `item` must be a `Scenario` (or a subclass thereof) or a test method of "
-                        "a scenario class (has to start with `test_`)")
+        raise TypeError("the given element for `item` must be a test method of a scenario class (has to start with "
+                        "`test_`)")
 
     class CoveredByDecorator:
         """decorator class for `@covered_by` decorator"""
@@ -38,26 +38,26 @@ def covered_by(item: Union[Type[Scenario], str, callable, None]):
                     raise TypeError(f"The decorator `@covered_by` may only be used for `Scenario` objects or for test "
                                     f"methods of one `Scenario` object. This is not possible for the applied class "
                                     f"`{func.__name__}`.")
-                scenario_controller = ScenarioController.get_for(func)
+                raise NotImplementedError('The covered-by decoration of other scenario classes is not supported yet')
+                # scenario_controller = ScenarioController.get_for(func)
                 # register for the whole class
-                scenario_controller.register_covered_by_for(meth=None, covered_by=item)
-            elif inspect.isfunction(func):
+                # scenario_controller.register_covered_by_for(meth=None, covered_by=item)
+            if inspect.isfunction(func):
                 # work will done in `__set_name__`
                 pass
             else:
                 raise TypeError(f"The use of the `@covered_by` decorator is not allowed for the `{str(func)}` element. "
-                                f"You should only use this decorator for `Scenario` elements or test method elements "
-                                f"of a `Scenario` object")
+                                f"You should only use this decorator for test method elements of a `Scenario` object")
 
         def __set_name__(self, owner, name):
             if issubclass(owner, Scenario):
                 if not inspect.isfunction(self.func):
-                    raise TypeError("the use of the `@covered_by` decorator is only allowed for `Scenario` objects and "
-                                    "test methods of `Scenario` objects")
+                    raise TypeError("the use of the `@covered_by` decorator is only allowed for test methods of "
+                                    "`Scenario` objects")
                 if not name.startswith('test_'):
-                    raise TypeError(f"the use of the `@covered_by` decorator is only allowed for `Scenario` objects "
-                                    f"and test methods of `Scenario` objects - the method `{owner.__name__}.{name}` "
-                                    f"does not start with `test_` and is not a valid test method")
+                    raise TypeError(f"the use of the `@covered_by` decorator is only allowed for test methods of "
+                                    f"`Scenario` objects - the method `{owner.__name__}.{name}` does not start with "
+                                    f"`test_` and is not a valid test method")
                 # if item is a string - resolve method
                 cleared_item = item
                 if isinstance(item, str):
@@ -66,8 +66,8 @@ def covered_by(item: Union[Type[Scenario], str, callable, None]):
                 scenario_controller.register_covered_by_for(meth=name, covered_by=cleared_item)
             else:
                 raise TypeError(f"The use of the `@covered_by` decorator is not allowed for methods of a "
-                                f"`{owner.__name__}`. You should only use this decorator for `Scenario` objects or "
-                                f"valid test methods of a `Scenario` object")
+                                f"`{owner.__name__}`. You should only use this decorator for valid test methods of a "
+                                f"`Scenario` object")
 
             setattr(owner, name, self.func)
 
