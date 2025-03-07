c8bf0d089f40e619606a00a9389499c3380c551c
matosys <max@matosys.de>
2025-03-07 07:44:40 +0100

    test(test_1_unsupported_class_decorator.py): add new test that checks error message in case `covered_by()` is used over scenario class

---
+++ tests/covered_by/test_1_unsupported_class_decorator/test_1_unsupported_class_decorator.py
@@ -0,0 +1,28 @@
+from multiprocessing import Process
+
+from _balder.testresult import ResultState
+from _balder.balder_session import BalderSession
+
+
+def test_1_calculator(balder_working_dir):
+    """
+    This testcase executes the basic example by using the `covered_by()` decorator on scenario class. This is currently
+    not supported, the test checks that an expected error message is thrown.
+    """
+    proc = Process(target=processed, args=(balder_working_dir, ))
+    proc.start()
+    proc.join()
+    assert proc.exitcode == 0, "the process terminates with an error"
+
+
+def processed(env_dir):
+    print("\n", flush=True)
+    session = BalderSession(cmd_args=[], working_dir=env_dir)
+    try:
+        session.run()
+        print("\n")
+        assert False, "test session terminates without an error"
+    except NotImplementedError as exc:
+        assert exc.args[0] == "The covered-by decoration of other scenario classes is not supported yet"
+
+    assert session.executor_tree is None, "test session does not terminates before collector work was done"
