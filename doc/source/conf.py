f3a57f9931c1787d155128a675d3e88d71bfaf97
matosys <max@matosys.de>
2025-10-22 10:50:01 +0200

    docs(index.rst): rework intro page with new design

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -81,6 +81,8 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = ['custom.css']
+
 # -- mermaid
 
 mermaid_version = ""
