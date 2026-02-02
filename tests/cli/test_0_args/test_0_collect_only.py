fb23edbf957c4c44c9a8936b4fe2125797fcca8f
matosys <max@matosys.de>
2026-02-02 08:25:46 +0100

    tests(cli): add additional test for validating normal collect-only output

---
+++ tests/cli/test_0_args/test_0_collect_only.py
@@ -0,0 +1,30 @@
+import re
+from _balder.balder_session import BalderSession
+from ...test_utilities.base_0_envtester_class import Base0EnvtesterClass
+
+
+class Test0CollectOnly(Base0EnvtesterClass):
+    """
+    This testcase executes the basic ENV example with a specific command line argument ``--collect-only``.
+
+    The test checks that no tests are executed and the printed information is like expected.
+    """
+
+    @property
+    def cmd_args(self):
+        return ['--collect-only']
+
+    @property
+    def expected_data(self) -> tuple:
+        return tuple()
+
+    def validate_printed_output(self, stdout: str) -> bool:
+        assert self._check_header_of_stdout(stdout, 2, 2), \
+            f"problems within header output"
+        assert len(stdout.splitlines()) == 5, f"detect more output lines (={len(stdout.splitlines())}) than expected"
+        return True
+
+    @staticmethod
+    def validate_finished_session(session: BalderSession):
+        # make sure that  no executor tree was created
+        assert session.executor_tree is None, f"executor tree is not None"
