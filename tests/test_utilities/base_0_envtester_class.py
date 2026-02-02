8ca0d41f81cbd8e6638e8ba864e166334c7201f7
matosys <max@matosys.de>
2026-02-02 08:22:42 +0100

    tests(test_utilities): introduce optional mechanism to also validate cli output in envtester environment

--- tests/test_utilities/base_0_envtester_class.py
+++ tests/test_utilities/base_0_envtester_class.py
@@ -1,9 +1,12 @@
-from typing import List
+import re
+import tempfile
+from typing import List, Optional
 import sys
 import traceback
 from typing import Union
 from abc import ABC, abstractmethod
 import pathlib
+from contextlib import redirect_stdout
 from multiprocessing import Process, Queue
 from _balder.balder_session import BalderSession
 from _balder.console.balder import _console_balder_debug
@@ -34,6 +37,37 @@ class Base0EnvtesterClass(ABC):
         """returns the expected exit code the process should terminate"""
         return 0
 
+    def validate_printed_output(self, stdout: str) -> bool:
+        """
+        Optional callback to validate expected printed output information on stdout.
+        It should raise exceptions in case it detects any problems.
+
+        :return: True if the data is valid, False otherwise (while it is preferred to use exceptions)
+        """
+        return True
+
+    @classmethod
+    def _check_header_of_stdout(
+            cls,
+            stdout: str,
+            expected_scenarios: int,
+            expected_setups: int,
+            expected_variations: Optional[int] = None,
+            expected_discarded: Optional[int] = None
+    ) -> bool:
+        re_expression = r"""^\+[-]+\+
+\| BALDER Testsystem\s+\|
+\|  python version .+? \| balder version .+?\s*\|
+\+[-]+\+
+"""
+        re_expression += f"Collect {expected_setups} Setups and {expected_scenarios} Scenarios"
+
+        if expected_variations:
+            re_expression += f"\n  resolve them to {expected_variations} valid variations"
+            if expected_discarded:
+                re_expression += f" \({expected_discarded} discarded\)"
+        return re.compile(re_expression, re.DOTALL | re.MULTILINE).match(stdout) is not None
+
     @property
     def cmd_args(self):
         """returns the command line arguments that should be used in the process call"""
@@ -45,19 +79,27 @@ class Base0EnvtesterClass(ABC):
         """
         exec_log_queue = Queue(maxsize=0)
 
-        proc = Process(target=self.__class__.processed_method,
-                       args=(balder_working_dir, self.cmd_args, exec_log_queue,
-                             self.__class__.validate_finished_session, self.__class__.handle_balder_exception))
-        proc.start()
-        exec_list = []
-        while proc.is_alive() or not exec_log_queue.empty():
+        with tempfile.TemporaryDirectory() as tempdir:
+            temp_filepath = pathlib.Path(tempdir) / "stdout.txt"
 
-            if not exec_log_queue.empty():
-                new_data = exec_log_queue.get()
-                new_data["file"] = pathlib.Path(new_data["file"]).relative_to(balder_working_dir)
-                exec_list.append(new_data)
+            proc = Process(target=self.__class__.processed_method,
+                           args=(balder_working_dir, self.cmd_args, exec_log_queue, temp_filepath,
+                                 self.__class__.validate_finished_session, self.__class__.handle_balder_exception))
+            proc.start()
+            exec_list = []
+            while proc.is_alive() or not exec_log_queue.empty():
 
-        proc.join()
+                if not exec_log_queue.empty():
+                    new_data = exec_log_queue.get()
+                    new_data["file"] = pathlib.Path(new_data["file"]).relative_to(balder_working_dir)
+                    exec_list.append(new_data)
+
+            proc.join()
+
+            with open(temp_filepath, 'r', encoding="utf-8") as stdout_file:
+                process_stdout = stdout_file.read()
+            print(process_stdout)
+
         assert proc.exitcode == self.expected_exit_code, \
             f"the process terminates with unexpected exit code `{proc.exitcode}` - exit code " \
             f"`{self.expected_exit_code}` was expected"
@@ -81,8 +123,15 @@ class Base0EnvtesterClass(ABC):
                 # can not find possible matching
                 raise error1
 
+        assert self.validate_printed_output(process_stdout.strip()), f"detect issues with printed output"
+
     @staticmethod
-    def processed_method(env_dir, args: List[str], queue: Queue, cb_validate_finished_session, cb_balder_exception):
+    def processed_method(
+            env_dir,
+            args: List[str],
+            queue: Queue,
+            stdout_filepath: pathlib.Path,
+            cb_validate_finished_session, cb_balder_exception):
         """
         This is the processed method
 
@@ -91,6 +140,8 @@ class Base0EnvtesterClass(ABC):
         :param args: a list with all command line arguments that should be used for the balder call
 
         :param queue: the queue the :class:RuntimeObserver` uses to communicate with the pytest process
+
+        :param stdout_filepath: the file path to the file the stdout should be written to
 
         :param cb_validate_finished_session: a callback that validates the :class:`BalderSession`, after the session
                                              has finished
@@ -116,10 +167,11 @@ class Base0EnvtesterClass(ABC):
                 traceback.print_exception(*sys.exc_info())
                 exit(255)
 
-        print("\n", flush=True)
-        _console_balder_debug(cmd_args=args, working_dir=env_dir, cb_session_created=set_queue,
-                              cb_run_finished=cb_finished, cb_balder_exc=cb_balder_exc)
-        print("\n")
+        with open(stdout_filepath, "w", encoding="utf-8") as stdout_file:
+            with redirect_stdout(stdout_file):
+                _console_balder_debug(cmd_args=args, working_dir=env_dir, cb_session_created=set_queue,
+                                      cb_run_finished=cb_finished, cb_balder_exc=cb_balder_exc)
+
 
     @staticmethod
     @abstractmethod
